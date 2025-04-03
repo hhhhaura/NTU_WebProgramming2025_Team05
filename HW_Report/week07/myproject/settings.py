506bda34083ac35177b5f20bfa7b5e67c1277bdd
Zion Huang <zionhuang1107@gmail.com>
2025-04-03 22:04:50 +0800

    feat: add index.html and static files

--- HW_Report/week07/myproject/settings.py
+++ HW_Report/week07/myproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static')
+    BASE_DIR / 'static'
 ]
 
 # Default primary key field type
