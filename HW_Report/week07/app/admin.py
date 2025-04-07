b6565a0778ad408e6b4f0ad061c1e022c3bba9a5
Zion Huang <zionhuang1107@gmail.com>
2025-04-07 10:24:54 +0800

    feat: docker compose and mysql

--- HW_Report/week07/app/admin.py
+++ HW_Report/week07/app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Transaction
 
-# Register your models here.
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('debtor', 'creditor', 'amount', 'created_at')
--- HW_Report/week07/myproject/settings.py
+++ HW_Report/week07/myproject/settings.py
@@ -11,20 +11,23 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)03x$z=^pz@cj6hlo)12y47ldw-=eytcr$36pqyi%47afw7nh'
+SECRET_KEY = os.getenv(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-m)03x$z=^pz@cj6hlo)12y47ldw-=eytcr$36pqyi%47afw7nh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -83,8 +86,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
